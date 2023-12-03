ca6c5e1c9697c75438f480a09228a4536a7de797
Louise Rondia <louiserondia@gmail.com>
2023-12-03 01:52:33 +0100

    1 peu mieux

--- day_2/part1.ex
+++ day_2/part1.ex
@@ -1,47 +1,31 @@
-defmodule FileReader do
+defmodule AoC do
   def read_file(file_path) do
-    case File.read(file_path) do
-      {:ok, content} ->
-        result =
-          String.split(content, "\n")
-          |> Enum.map(fn x -> String.split(x, ":") end)
-          |> Enum.map(&parse_line/1)
-          |> Enum.filter(&(&1 != 0))
-          |> Enum.sum()
-
-        IO.inspect(result, label: "Result ")
-
-      {:error, reason} ->
-        IO.puts("Erreur lors de la lecture du fichier : #{reason}")
+    with {:ok, content} <- File.read(file_path) do
+      String.split(content, "\n")
+      |> Enum.map(&String.split(&1, ":"))
+      |> Enum.map(&solve/1)
+      |> Enum.sum()
     end
   end
 
-  defp too_much(n, color) do
-    case [n, color] do
-      [n, "red"] when n > 12 -> true
-      [n, "green"] when n > 13 -> true
-      [n, "blue"] when n > 14 -> true
-      _ -> false
-    end
-  end
+  defp check(n, "red") when n > 12, do: true
+  defp check(n, "green") when n > 13, do: true
+  defp check(n, "blue") when n > 14, do: true
+  defp check(_, _), do: false
 
-  defp parse_line([head | tail]) do
+  defp solve([head | tail]) do
     trimed = String.replace(head, "Game ", "") |> String.to_integer()
 
-    splited =
+    res =
       Enum.join(tail)
       |> String.split(~r/[;,]/)
       |> Enum.map(&String.trim/1)
       |> Enum.map(&String.split(&1, " "))
       |> Enum.map(fn [head | rest] -> [String.to_integer(head) | rest] end)
+      |> Enum.map(fn [head | rest] -> check(head, Enum.join(rest)) end)
 
-    res = Enum.map(splited, fn [head | rest] -> too_much(head, Enum.join(rest)) end)
-
-    case Enum.any?(res, & &1) do
-      true -> 0
-      false -> trimed
-    end
+    if Enum.any?(res), do: 0, else: trimed
   end
 end
 
-FileReader.read_file("input/part1.txt")
+IO.puts("Result : #{AoC.read_file("input/part1.txt")}")
--- day_2/part2.ex
+++ day_2/part2.ex
@@ -1,23 +1,14 @@
-defmodule FileReader do
+defmodule AoC do
   def read_file(file_path) do
-    case File.read(file_path) do
-      {:ok, content} ->
-        result =
-          String.split(content, "\n")
-          |> Enum.map(fn x -> String.split(x, ":") end)
-          |> Enum.map(&parse_line/1)
-          |> Enum.sum()
-
-        IO.inspect(result, label: "Result ")
-
-      {:error, reason} ->
-        IO.puts("Erreur lors de la lecture du fichier : #{reason}")
+    with {:ok, content} <- File.read(file_path) do
+      String.split(content, "\n")
+      |> Enum.map(&String.split(&1, ":"))
+      |> Enum.map(&solve/1)
+      |> Enum.sum()
     end
   end
 
-  defp biggest([], red, green, blue) do
-    red * green * blue
-  end
+  defp biggest([], red, green, blue), do: red * green * blue
 
   defp biggest([head | tail], red, green, blue) do
     case head do
@@ -28,7 +19,7 @@ defmodule FileReader do
     end
   end
 
-  defp parse_line([_head | tail]) do
+  defp solve([_head | tail]) do
     splited =
       Enum.join(tail)
       |> String.split(~r/[;,]/)
@@ -36,10 +27,8 @@ defmodule FileReader do
       |> Enum.map(&String.split(&1, " "))
       |> Enum.map(fn [head | rest] -> [String.to_integer(head) | rest] end)
 
-    case splited do
-      [head | tail] -> biggest([head | tail], 0, 0, 0)
-    end
+    biggest(splited, 0, 0, 0)
   end
 end
 
-FileReader.read_file("input/part1.txt")
+IO.puts("Result : #{AoC.read_file("input/part1.txt")}")
