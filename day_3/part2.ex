7b1a4d8cf762395593395e40b0c9b07a016b7388
Louise Rondia <louiserondia@gmail.com>
2023-12-04 17:19:52 +0100

    merci le v

--- day_3/part2.ex
+++ day_3/part2.ex
@@ -1,6 +1,6 @@
 defmodule AoC do
-  defp find_coord([b, m1, m2, e], x, y) do
-    case Enum.find([b, m1, m2, e], &String.contains?(&1, "*")) do
+  defp find_coord([up, left, right, down], x, y, len) do
+    case Enum.find([up, left, right, down], &String.contains?(&1, "*")) do
       nil ->
         {0, 0}
 
@@ -8,10 +8,10 @@ defmodule AoC do
         i = Regex.scan(~r/\*/, star, return: :index) |> hd |> Enum.at(0) |> elem(0)
 
         case star do
-          ^b -> {max(x + i - 1, i), y - 1}
-          ^m1 -> {x - 1, y}
-          ^m2 -> {x + 1, y}
-          ^e -> {max(x + i - 1, i), y + 1}
+          ^up -> {max(x + i - 1, i), y - 1}
+          ^left -> {x - 1, y}
+          ^right -> {x + len, y}
+          ^down -> {max(x + i - 1, i), y + 1}
         end
     end
   end
@@ -37,13 +37,13 @@ defmodule AoC do
           ]
       end
 
-    coord = find_coord(res, x, y)
+    coord = find_coord(res, x, y, String.length(number))
     {number, coord}
   end
 
-  defp solve(_prev, [], _y), do: 0
+  defp solve(_prev, [], _y, l), do: l
 
-  defp solve(prev, [current | next], y) do
+  defp solve(prev, [current | next], y, l) do
     matches = Regex.scan(~r/\d+/, current)
     index = Regex.scan(~r/\d+/, current, return: :index) |> Enum.map(fn [{i, _b}] -> i end)
     fusion = Enum.zip(matches, index)
@@ -51,22 +51,20 @@ defmodule AoC do
     res =
       Enum.map(fusion, &check(prev, [current | next], &1, y))
       |> Enum.filter(fn {_value, {x, y}} -> {x, y} != {0, 0} end)
-      |> Enum.filter(&(length(&1) != 0))
-      # |> Enum.group_by(&elem(&1, 1))
-      # |> Enum.filter(&length(elem(&1, 1)) > 1)
-      # |> Map.values()
-      # |> Enum.filter(fn {_key, values} -> length(values) > 1 end)
-      # |> Enum.flat_map(&tl(&1))
-      # |> Enum.map(&elem(&1, 0))
 
-    IO.inspect(res)
-    solve(current, next, y + 1)
+    solve(current, next, y + 1, l ++ res)
   end
 
   def read_file(file_path) do
     with {:ok, content} <- File.read(file_path) do
       lines = String.split(content)
-      solve(nil, lines, 0)
+
+      solve(nil, lines, 0, [])
+      |> Enum.group_by(&elem(&1, 1))
+      |> Map.values()
+      |> Enum.filter(&(&1 |> length == 2))
+      |> Enum.map(fn [{n1, _}, {n2, _}] -> String.to_integer(n1) * String.to_integer(n2) end)
+      |> Enum.sum()
     end
   end
 end
