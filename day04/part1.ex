2ac358d1e91aabba7b6ab07b1108f434d70a0090
Louise Rondia <louiserondia@gmail.com>
2023-12-05 01:38:08 +0100

    day4 part1

---
+++ day04/part1.ex
@@ -0,0 +1,23 @@
+defmodule AoC do
+  def read_file(file_path) do
+    with {:ok, content} <- File.read(file_path) do
+      String.split(content, "\n", trim: true)
+      |> Enum.map(fn line -> String.split(line, ":") |> Enum.at(1) end)
+      |> Enum.map(&String.split(&1, "|", trim: true))
+      |> Enum.map(fn line ->
+        Enum.map(line, fn each_side ->
+          String.split(each_side)
+          |> Enum.map(&String.to_integer(&1))
+        end)
+      end)
+      |> Enum.map(fn [left, right] ->
+        Enum.map(left, fn el -> if Enum.member?(right, el), do: el end)
+        |> Enum.filter(&(&1 != nil))
+      end)
+      |> Enum.map(&(:math.pow(2, length(&1) - 1) |> floor()))
+      |> Enum.sum()
+    end
+  end
+end
+
+IO.puts("Result : #{AoC.read_file("input/part1.txt")}")
