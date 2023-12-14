5bd23bafce96fcf26f88ffba0454328419c923c8
Louise Rondia <louiserondia@gmail.com>
2023-12-14 13:12:07 +0100

    marche

--- day12/part1.ex
+++ day12/part1.ex
@@ -15,44 +15,26 @@ defmodule AoC do
     res = (elem(res, 1) ++ [elem(res, 2)]) |> Enum.filter(&(&1 != 0))
 
     cond do
-      n != distrib |> Enum.sum() ->
-        [""]
-
-      res != distrib ->
-        [""]
-
-      true ->
-        [l |> Enum.join()]
+      n != distrib |> Enum.sum() -> [""]
+      res != distrib -> [""]
+      true -> [l |> Enum.join()]
     end
   end
 
-  defp calc(s, n, hs, pos, prev) do
-    p = Enum.at(hs, pos)
+  defp generate(s, i, n) do
+    if i == String.length(s) do
+      check_line(s, n)
+    else
+      c = String.at(s, i)
 
-    res =
-      s
-      |> String.graphemes()
-      |> Enum.reduce({0, ""}, fn
-        c, {i, acc} ->
-          case {i, c} do
-            {^p, "."} -> {i + 1, acc <> "#"}
-            {^p, "#"} -> {i + 1, acc <> "."}
-            {_, "?"} -> {i + 1, acc <> "#"}
-            {_, a} -> {i + 1, acc <> a}
-          end
-      end)
-      |> elem(1)
+      case c do
+        "?" ->
+          generate(String.replace(s, "?", "#", global: false), i + 1, n) ++
+            generate(String.replace(s, "?", ".", global: false), i + 1, n)
 
-    cond do
-      prev >= -1 ->
-        if pos < length(hs) - 1 do
-          check_line(res, n) ++ calc(res, n, hs, pos + 1, prev)
-        else
-          check_line(res, n) ++ calc(res, n, hs, prev - 1, prev - 1)
-        end
-
-      true ->
-        check_line(res, n)
+        _ ->
+          generate(s, i + 1, n)
+      end
     end
   end
 
@@ -64,16 +46,9 @@ defmodule AoC do
       |> Enum.map(&String.split(&1))
       |> Enum.map(fn [s, n] -> [s, String.split(n, ",") |> Enum.map(&String.to_integer(&1))] end)
       |> Enum.map(fn [s, n] ->
-        hs =
-          Enum.with_index(String.graphemes(s))
-          |> Enum.filter(fn {c, _} -> c == "?" end)
-          |> Enum.map(fn {_, i} -> i end)
-
-        calc(s, n, hs, length(hs), length(hs))
-        |> Enum.filter(&(&1 != ""))
-        |> Enum.uniq()
-        |> Enum.count()
+        generate(s, 0, n) |> Enum.filter(&(&1 != "")) |> Enum.count()
       end)
+
       |> Enum.sum()
     end
   end
