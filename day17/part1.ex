985baf71974b1e8611519385592cdde74383d905
Louise Rondia <louiserondia@gmail.com>
2024-01-11 18:20:39 +0100

    aaaa

--- day17/part1.ex
+++ day17/part1.ex
@@ -1,5 +1,4 @@
 defmodule AoC do
-
   defp map2d(s) do
     s
     |> Enum.with_index()
@@ -13,28 +12,58 @@ defmodule AoC do
     |> Enum.reduce(%{}, fn {k, v}, acc -> acc |> Map.put(k, v) end)
   end
 
-  defp calc({x, y}, prev, count, res, dirs, data, cache) do
-    verif = Agent.get(:res, fn r -> r[{{x, y}, elem(count, 0)}] end) #soit rien, soit elem(count, 0), soit count
+  defp print(pos, cache) do
+    IO.inspect(pos)
+    n = cache[pos] |> elem(1)
+    if n, do: print(n, cache)
+  end
 
-    if !verif || res <= verif do
-      Agent.update(:res, fn r -> r |> Map.put({{x, y}, elem(count, 0)}, res) end)
+  defp count({x, y}, dir, i, cache) do
+    prev = cache[{x, y}] |> elem(1)
 
-      cache = cache |> MapSet.put({x, y})
+    case {dir, prev} do
+      {{0, 0}, {^x, py}} -> count(prev, {0, y - py}, 1, cache)
+      {{0, 0}, {px, ^y}} -> count(prev, {x - px, 0}, 1, cache)
+      {{0, _}, {^x, _}} -> count(prev, dir, i + 1, cache)
+      {{_, 0}, {_, ^y}} -> count(prev, dir, i + 1, cache)
+      _ -> {dir, i}
+    end
+  end
 
+  defp traverse({x, y}, dirs, data, cache, {w, h}) do
+    {pdir, i} = count({x, y}, {0, 0}, 0, cache)
+
+    next =
       dirs
-      |> Enum.filter(fn {nx, ny} ->
-        data[{x + nx, y + ny}] && not (cache |> MapSet.member?({x + nx, y + ny})) &&
-          {x + nx, y + ny} != prev
-      end)
-      |> Enum.map(fn n ->
-        ncoord = {x + elem(n, 0), y + elem(n, 1)}
+      |> Enum.filter(fn {x1, y1} -> data[{x + x1, y + y1}] end)
+      |> Enum.filter(fn dir -> dir != pdir || i != 3 end)
+      |> Enum.map(fn {x1, y1} -> {x + x1, y + y1} end)
 
-        case count do
-          {^n, 3} -> nil
-          {^n, i} -> calc(ncoord, {x, y}, {n, i + 1}, res + data[ncoord], dirs, data, cache)
-          _ -> calc(ncoord, {x, y}, {n, 1}, res + data[ncoord], dirs, data, cache)
+    data = data |> Map.delete({x, y})
+    cur_dist = cache[{x, y}] |> elem(0)
+
+    cache =
+      next
+      |> Enum.reduce(cache, fn n, acc ->
+        next_dist = cur_dist + data[n]
+
+        if is_nil(acc[n]) || next_dist < acc[n] |> elem(0) do
+          acc |> Map.put(n, {next_dist, {x, y}})
+        else
+          acc
         end
       end)
+
+    if Enum.empty?(data) do
+      cache
+    else
+      next_dir =
+        cache
+        |> Enum.filter(fn {k, _} -> data[k] end)
+        |> Enum.min_by(fn {_, {v, _}} -> v end)
+        |> elem(0)
+
+      traverse(next_dir, dirs, data, cache, {w, h})
     end
   end
 
@@ -47,29 +76,22 @@ defmodule AoC do
         |> String.split("\n", trim: true)
         |> map2d()
 
-      [w, h] =
+      {w, h} =
         data
-        |> Enum.reduce([0, 0], fn {{x, y}, _}, [w, h] ->
-          [(if x > w, do: x, else: w), (if y > h, do: y, else: h)]
+        |> Enum.reduce({0, 0}, fn {{x, y}, _}, {w, h} ->
+          {if(x > w, do: x, else: w), if(y > h, do: y, else: h)}
         end)
 
-        dirs = [
+      dirs = [
         {0, -1},
         {0, 1},
         {-1, 0},
         {1, 0}
       ]
 
-      Agent.start_link(fn -> %{} end, name: :res)
-
-      calc({0, 0}, {0, 0}, {{0, 0}, 0}, 0, dirs, data, MapSet.new())
-
-      Agent.get(:res, & &1)
-      |> Enum.filter(fn {{coord, _}, _} -> coord == {w, h} end)
-      |> Enum.sort(:desc)
-      |> Enum.map(fn {_, v} -> v end)
-      |> Enum.min()
-
+      r = traverse({0, 0}, dirs, data, %{{0, 0} => {0, nil}}, {w, h})
+      print({w, h}, r)
+      r[{w, h}] |> elem(0)
 
     end
   end
