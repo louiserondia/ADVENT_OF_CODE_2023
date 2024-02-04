42af6d26414de62dd7c489d6e1bbb2b008e9ff14
Louise Rondia <louiserondia@gmail.com>
2024-02-04 02:06:55 +0100

    on verra demain

--- day17/part1.ex
+++ day17/part1.ex
@@ -18,52 +18,44 @@ defmodule AoC do
     if n, do: print(n, cache)
   end
 
-  defp count({x, y}, dir, i, cache) do # counts how many times we've gone in a certain dir
-    prev = cache[{x, y}] |> elem(1)
-
-    case {dir, prev} do
-      {{0, 0}, {^x, py}} -> count(prev, {0, y - py}, 1, cache)
-      {{0, 0}, {px, ^y}} -> count(prev, {x - px, 0}, 1, cache)
-      {{0, _}, {^x, _}} -> count(prev, dir, i + 1, cache)
-      {{_, 0}, {_, ^y}} -> count(prev, dir, i + 1, cache)
-      _ -> {dir, i}
-    end
-  end
-
-  defp traverse({x, y}, dirs, data, cache, {w, h}) do
-    {pdir, i} = count({x, y}, {0, 0}, 0, cache) # get the dir we came from and how many times we've gone that way
+  defp traverse({x, y}, {dx, dy}, dirs, data, cache, {w, h}) do
 
     next = # Get the next possible directions, checks if they exist and if we haven't been that way 3 times
       dirs
       |> Enum.filter(fn {x1, y1} -> data[{x + x1, y + y1}] end)
-      |> Enum.filter(fn dir -> dir != pdir || i != 3 end)
-      |> Enum.map(fn {x1, y1} -> {x + x1, y + y1} end)
-
-    data = data |> Map.delete({x, y})
-    cur_dist = cache[{x, y}] |> elem(0)
+      |> Enum.filter(fn dir -> not (dir == {dx, dy} && (abs(dx) == 3 || abs(dy) == 3)) end)
+      # |> Enum.map(fn {x1, y1} -> {x + x1, y + y1} end)
 
     cache = # Updating cache with all scores, if we have a new way to go somewhere cheaper, it's updated in the cache
       next
-      |> Enum.reduce(cache, fn n, acc ->
-        next_dist = cur_dist + data[n]
+      |> Enum.reduce(cache, fn {nx, ny}, acc ->
+        new_pos = {x + nx, y + ny}
+        new_score = cache[{{x, y}, {dx, xy}}] + data[new_pos]
+        next_dir = case {nx, ny} do
+          {0, ^dy} -> {0, (abs(dy) + 1) * y} #2e fois la même en verticale
+          {^dx, 0} -> {(abs(dx) + 1) * x, 0}
+          {^dx, y} when y < abs(dy) -> {0, (abs(dy) + 1) * y} #3e fois
+          {x, ^dy} when x < abs(dx) -> {(abs(dx) + 1) * x, 0}
+          {x, y} -> {x, y} # 1e fois, quand nx et ny sont != de dx, dy
+        end
 
-        if is_nil(acc[n]) || next_dist < acc[n] |> elem(0) do
-          acc |> Map.put(n, {next_dist, {x, y}})
+        if is_nil(acc[new_pos]) || new_score < acc[new_pos] |> elem(0) do
+          acc |> Map.put({new_pos, next_dir}, new_score)
         else
           acc
         end
       end)
 
-    if Enum.empty?(data) do
+    if  do #si on a une valeur a cache[{w, h}, _] utiliser find ?
       cache
     else
-      next_dir = # Get the next spot to go to, it has to exist and be the lowest score and lowest dir ??
+      {npos, ndir} = # Get the next spot to go to, it has to exist and be the lowest score and lowest dir ??
         cache
-        |> Enum.filter(fn {k, _} -> data[k] end)
-        |> Enum.min_by(fn {_, {v, _}} -> v end)
+        |> Enum.filter(fn {{coord, _}, _} -> data[coord] end)
+        |> Enum.min_by(fn {_, v} -> v end)
         |> elem(0)
 
-      traverse(next_dir, dirs, data, cache, {w, h})
+      traverse(npos, ndir, dirs, data, cache, {w, h})
     end
   end
 
@@ -89,9 +81,10 @@ defmodule AoC do
         {1, 0}
       ]
 
-      r = traverse({0, 0}, dirs, data, %{{0, 0} => {0, nil}}, {w, h})
-      print({w, h}, r)
-      r[{w, h}] |> elem(0)
+      # r =
+         traverse({0, 0}, {0, 0}, dirs, data, %{{{0, 0}, nil} => 0}, {w, h})
+      # print({w, h}, r)
+      # r[{w, h}] |> elem(0)
 
     end
   end
